df8464b5771281e64eabb2cd99b53e94a1731fb8
Essam Ahmed <essam.anbar11@gmail.com>
2020-02-12 01:02:44 +0200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+/*===========================================================
+                            Services
+=============================================================*/
+$(function () {
+    //animate on scroll
+    new WOW().init();
+});
+/*===========================================================
+                      Work               
+=============================================================*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+/*===========================================================
+                         Team            
+=============================================================*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+/*===========================================================
+                         Testimonials            
+=============================================================*/
+$(function () {
+    $("#customer-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+/*===========================================================
+                                 Stats    
+=============================================================*/
+$(document).ready(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+/*===========================================================
+                             Clients        
+=============================================================*/
+$(function () {
+    $("#clients").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
